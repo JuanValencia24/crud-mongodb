c2a686c5eaa9a5ae586c99d7d74adf8221fe247b
JuanValencia24 <viafarajuan2@gmail.com>
2023-05-18 09:17:49 -0500

    creacion de proyecto proyecto

---
+++ src/main/java/com/co/jv/api_mongobd/ApiMongobdApplication.java
@@ -0,0 +1,13 @@
+package com.co.jv.api_mongobd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMongobdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMongobdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/jv/api_mongobd/ApiMongobdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.jv.api_mongobd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMongobdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
